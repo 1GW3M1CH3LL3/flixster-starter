3603531a935f1db94c3b975caee69f28f494bce5
Adaora Michelle Igwe <adaoraigwe2006@meta.com>
2025-06-12 12:57:44 -0700

    renders and implements add to  watch button

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 
 function MovieCard(props) {
   const [isLike, setIsLike] = useState(false);
+  const [isWatched, setIsWatched] = useState("Add To Watched");
+  const handleWatchClick = (e) => {
+    e.stopPropagation();
+    setIsWatched(
+      isWatched === "Add To Watched" ? "Watched!" : "Add To Watched"
+    );
+  };
   const modalObject = {
     poster: props.poster,
     title: props.moviename,
@@ -22,18 +29,25 @@ function MovieCard(props) {
 
   return (
     <div onClick={modalDisplay} className="card">
-      <img
-        // height="250px"
-        width="180px"
-        src={"https://image.tmdb.org/t/p/w500" + props.poster}
-        alt="poster"
-      />
-      <p>{props.moviename}</p>
-      <p>{props.rating}</p>
+      <div className="name-rating">
+        <img
+          className="movie-image"
+          // height="250px"
+          // width="180px"
+          src={"https://image.tmdb.org/t/p/w500" + props.poster}
+          alt={props.moviename + " poster"}
+        />
+        <p>{props.moviename}</p>
+        <p>{props.rating}</p>
+      </div>
+
       <i
         onClick={like}
         className={`fa-solid fa-heart ${isLike ? "like" : ""}`}
       ></i>
+      <button onClick={handleWatchClick} className="add-to-watched">
+        {isWatched}
+      </button>
     </div>
   );
 }
